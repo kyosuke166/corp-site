6e8cb9be6e7737dfc38a5702e10b5a6074836f99
kyosuke166 <kyosuke166@gmail.com>
2026-02-07 00:02:33 +0900

    feat: implement system page and complete site skeleton

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const SITE_DATA = {
+  companyName: "株式会社ＳＢＴ",
+  representative: "柴田 雄介",
+  foundingDate: "2015年6月3日",
+  postalCode: "170-0011",
+  address: "東京都豊島区池袋本町4-11-1",
+  tel: "03-6869-3615",
+  fax: "03-6869-3616",
+  googleMapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.931720817006!2d139.70511637635677!3d35.73905957256863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d5062569501%3A0x9599d14620f4f959!2z44CSMTcwLTAwMTEg5p2x5Lqs6YO96LGK5bO25Yy65rGg6KKL5pys55S677yU5LiB55uu77yR77yR4oiS77yR!5e0!3m2!1sja!2sjp!4v1716440000000!5m2!1sja!2sjp"
+};
